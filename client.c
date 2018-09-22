51a9e106de2a255e73fdf4fa84dd1e04a42b61f2
Benjamin Saljooghi <benjamin.saljooghi@gmail.com>
2018-09-22 13:25:54 +1000

    Add login prompting and menu

--- client.c
+++ client.c
@@ -33,7 +33,68 @@ void draw_field()
     }
 }
 
+void login()
+{
+    printf("====================\n");
+    printf("Welcome to the online Minesweeper gaming system\n");
+    printf("====================\n\n");
+    printf("You are required to login with your registered username and password.\n\n");
+    
+    char username[255];
+    char password[255];
+
+    printf("Enter your username: ");
+    scanf("%255s", username);  
+    printf("Enter your password: ");
+    scanf("%255s", password);
+}
+
+int menu()
+{
+    printf("Welcome to the Minesweeper gaming system.\n\n");
+    printf("Please enter a selection:\n");
+    printf("<1> Play Minesweeper\n");
+    printf("<2> Show Leaderboard\n");
+    printf("<3> Quit\n\n");
+
+    int selection;
+
+    do
+    {
+        printf("Selection option (1-3):");
+        scanf("%d", &selection);
+    } while (selection < 1 || selection > 3);
+    
+    return selection;
+}
+
 int main(int argc, char* argv[])
 {
-    draw_field();
+    printf("\n");
+
+    char* ip_address = argv[1];
+    int port = atoi(argv[2]);
+
+    printf("ip_address: %s\n", ip_address);
+    printf("port: %d\n", port);
+
+    login();
+    int selection = menu();
+
+    switch (selection)
+    {
+        case 1:
+            draw_field();
+            break;
+
+        case 2:
+            draw_field();
+            break;
+
+        case 3:
+            return 0;
+            break;
+    }
+
+    return 0;
 }
