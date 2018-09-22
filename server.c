26f566b6ec491315b8a6398c4f47773e2fb89062
Jacob Marks <jacobjmarks@outlook.com>
2018-09-22 15:04:10 +1000

    Setup serverside mine placement

--- server.c
+++ server.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 #include "constants.h"
 #include "tile.h"
 
@@ -10,18 +11,41 @@ typedef struct {
     Tile tiles[NUM_TILES_X][NUM_TILES_Y];
 } GameState;
 
-void place_mines(Tile*** tiles) {
+void print_tile_state(Tile tiles[NUM_TILES_X][NUM_TILES_Y]) {
+    printf("[adjacent_mines revealed is_mine]\n");
+    for (int x = 0; x < NUM_TILES_X; x++) {
+        for (int y = 0; y < NUM_TILES_Y; y++) {
+            Tile tile = tiles[x][y];
+            printf("[%d %d %d] ", tile.adjacent_mines, tile.revealed, tile.is_mine);
+        }
+        printf("\n");
+    }
+}
+
+void place_mines(Tile tiles[NUM_TILES_X][NUM_TILES_Y]) {
     for (int i = 0; i < NUM_MINES; i++) {
         int x_pos, y_pos;
-        Tile tile;
+        Tile* tile;
 
         do {
             x_pos = rand() % NUM_TILES_X;
             y_pos = rand() % NUM_TILES_Y;
-            tile = *tiles[x_pos][y_pos];
-        } while(tile.is_mine);
+            tile = &tiles[x_pos][y_pos];
+        } while(tile->is_mine);
 
-        tile.is_mine = true;
+        tile->is_mine = true;
+    }
+}
+
+void serve_client() {
+    GameState* gs = malloc(sizeof(GameState));
+
+    place_mines(gs->tiles);
+
+    print_tile_state(gs->tiles);
+
+    while (true) {
+        // Listen
     }
 }
 
@@ -31,9 +55,12 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    const int port = stoi(argv[1]);
+    srand(time(NULL));
+
+    const int port = atoi(argv[1]);
 
     // Listen
+    serve_client();
 
     return 0;
 }
