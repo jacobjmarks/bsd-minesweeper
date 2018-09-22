b19534a335ac091c99a451780933ec0582ae627b
Jacob Marks <jacobjmarks@outlook.com>
2018-09-22 18:00:27 +1000

    Implemented basic server listening and response

--- server.c
+++ server.c
@@ -3,6 +3,9 @@
 #include <stdbool.h>
 #include <time.h>
 #include <string.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
 #include "constants.h"
 #include "tile.h"
 
@@ -89,17 +92,65 @@ bool authenticate(char* user, char* pass) {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        printf("Please provide server port.");
+    if (argc != 2) {
+        fprintf(stderr, "Usage: ./server.o PORT\n");
         return 1;
     }
-
+    
     srand(time(NULL));
 
     const int port = atoi(argv[1]);
 
-    // Listen
-    serve_client();
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+       
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    while (true) {
+        char buffer[1024] = {0};
+        valread = read(new_socket, buffer, 1024);
+            
+        char* user = strtok(buffer, "\t");
+        char* pass = strtok(NULL, "\n");
+
+        printf("%s %s\n", user, pass);
+
+        int response = htonl(authenticate(user, pass));
+
+        send(new_socket, &response, sizeof(response), 0);
+    }
 
     return 0;
 }
