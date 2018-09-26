3d178d37cde019b74e32b44331dcbe981ccccd46
Benjamin Saljooghi <benjamin.saljooghi@gmail.com>
2018-09-26 18:31:59 +1000

    Client: ctoi itoc, spunk/eavesdrop split, tile request/read

--- client.c
+++ client.c
@@ -4,15 +4,20 @@
 #include <netinet/in.h> 
 #include <string.h> 
 #include <stdbool.h>
+#include <unistd.h>
 
 #include "constants.h"
 #include "tile.h"
 #include "protocol.h"
 
+#define ctoi(my_char) (my_char-'0')
+#define itoc(my_int)  (my_int+'0')
+
 char field[NUM_TILES_X][NUM_TILES_Y];
 
-void update_tile(int x, int y, char c)
+void update_tile(int x, int y, const char c)
 {
+    printf("Attempting to update x:%d y:%d with %c\n", x, y, c);
     field[x][y] = c;
 }
 
@@ -38,7 +43,7 @@ void draw_field()
     printf("\n");
 }
 
-void game()
+void game(int sock)
 {
     char option;
     char position[2];
@@ -46,33 +51,60 @@ void game()
     while(1)
     {
         draw_field();
-
         printf("Select option <P, R, Q>: ");
         scanf(" %c", &option);
-
         if(option == 'Q')
             return;
-
         printf("Select position: ");
         scanf(" %s2", position);
 
-        int x_pos = position[1] - '0' - 1;
+        int protocol = option == 'P' ? FLAG_TILE : REVEAL_TILE;
+        
+        int x_pos = ctoi(position[1]) - 1;
         int y_pos = ((int) position[0]) - 65;
 
-        update_tile(x_pos, y_pos, '+');
+        char pos_request[BUFFER_SIZE];
+        pos_request[0] = x_pos;
+        pos_request[1] = y_pos; 
+
+        spunk(sock, protocol, pos_request);
+        
+        while(1)
+        {
+            char response[BUFFER_SIZE];
+            eavesdrop(sock, response);
+            if (response[0] == 'T')
+                break;
+            update_tile(ctoi(response[0]), ctoi(response[1]), response[3]);
+        }
+        
     }
 }
 
-int spunk(int sock, int protocol, char* message, char* response)
+int eavesdrop(int sock, char* response)
+{
+    if (read(sock, response, BUFFER_SIZE) <= 0)
+    {
+        printf("Connection failure.\n");
+        exit(1);
+    }
+    return 0;
+}
+
+int spunk(int sock, int protocol, const char* message)
 {
     char packet[BUFFER_SIZE];
-    packet[0] = protocol + '0';
-    strncat(packet, message, 50);
-    printf("Sending '%s' with size %d on sock %d\n", packet, BUFFER_SIZE, sock);
-    write(sock, packet, BUFFER_SIZE);
-    read(sock, response, BUFFER_SIZE);
-    int status = 0;
-    return status;
+    packet[0] = itoc(protocol);
+    strncat(packet, message, 99);
+    printf("Sending '%s' with size %d... ", packet, BUFFER_SIZE);
+    
+    if (write(sock, packet, BUFFER_SIZE) < 0)
+    {
+        printf("Connection failure.\n");
+        exit(1);
+    }
+
+    return 0;
 }
 
 int login(char* ip, int port)
@@ -85,62 +117,66 @@ int login(char* ip, int port)
     int sock = 0;
 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-    { 
-        printf("\nSocket creation error.\n"); 
+    {
+        printf("\nSocket creation error.\n");
         exit(1);
-    } 
-   
+    }
+
     memset(&serv_addr, '0', sizeof(serv_addr)); 
     serv_addr.sin_family = AF_INET; 
     serv_addr.sin_port = htons(port); 
        
     if(inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0)  
-    { 
+    {
         printf("\nInvalid address or address not supported.\n"); 
-        exit(1); 
-    } 
-   
+        exit(1);
+    }
+
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        printf("\nConnection failed.\n"); 
-        exit(1); 
+    {
+        printf("\nConnection failed.\n");
+        exit(1);
     }
 
     printf(" Connection established.\n");
-
-    printf
-    (
-        "You are required to login with your registered username and password.\n\n"
-    );
+    printf("You are required to login with your registered username and password.\n\n");
 
     char username[10];
     char password[10];
-    char message[BUFFER_SIZE];
 
     printf("Enter your username: ");
     scanf(" %10s", username);  
     printf("Enter your password: ");
     scanf(" %10s", password);
 
-    strncat(message, username, 10);
-    strncat(message, "\t", 2);
-    strncat(message, password, 10);
+    char credentials[BUFFER_SIZE];
+    sprintf(credentials,"%s:%s", username, password);
+    
+    spunk(sock, LOGIN, credentials);
+    char response[BUFFER_SIZE];
+    eavesdrop(sock, response);
 
-    char buffer[BUFFER_SIZE];
-    spunk(sock, LOGIN, message, buffer);
-    printf("Response: %s", buffer);
+    if (response[0])
+    {
+        return sock;
+    }
+    
+    printf("Login failure.\n");
 
-    return sock;
+    exit(1);
 }
 
 int menu()
 {
-    printf("\nWelcome to the Minesweeper gaming system.\n\n");
-    printf("Please enter a selection:\n");
-    printf("<1> Play Minesweeper\n");
-    printf("<2> Show Leaderboard\n");
-    printf("<3> Quit\n\n");
-
+    printf
+        (
+            "\nWelcome to the Minesweeper gaming system.\n\n"
+            "Please enter a selection:\n"
+            "<1> Play Minesweeper\n"
+            "<2> Show Leaderboard\n"
+            "<3> Quit\n\n"
+        );
+           
     int selection;
     do
     {
@@ -152,10 +188,6 @@ int menu()
 
 int main(int argc, char* argv[])
 {
-
-    // char response[100];
-    // printf("%d\n", strlen(response));
-
     char* ip = argv[1];
     int port = atoi(argv[2]);
     int sock = login(ip, port);
@@ -163,7 +195,7 @@ int main(int argc, char* argv[])
     switch (menu())
     {
         case 1:
-            game(&sock);
+            game(sock);
             break;
 
         case 2:
