53a282a2f30a71d73f25c9f5a11ae3f73260c768
Benjamin Saljooghi <benjamin.saljooghi@gmail.com>
2018-10-25 22:12:00 +1000

    Run clang-format for same-line braces

--- client.c
+++ client.c
@@ -1,21 +1,21 @@
 /*******************************************************************************
  * client.c
- * 
+ *
  * Client program for playing Minesweeper.
- * 
+ *
  * Author: Benjamin Saljooghi n9448233
  ******************************************************************************/
 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
-#include <string.h> 
-#include <stdbool.h>
-#include <unistd.h>
-#include <time.h>
 #include <arpa/inet.h>
 #include <ctype.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "common.h"
 
@@ -37,9 +37,8 @@ typedef struct HighScore {
 
 /**
  * Reports a connection failure to the user and terminates the client.
- */ 
-void connection_failure()
-{
+ */
+void connection_failure() {
     printf("Connection failure.\n");
     exit(1);
 }
@@ -47,14 +46,12 @@ void connection_failure()
 /**
  * Sends an int to the server. Gracefully terminates the client
  * if no connection could be made.
- * 
+ *
  * fd: the file descriptor for the socket.
  * message: the int to be sent.
- */ 
-void send_int_check(int fd, uint32_t message)
-{
-    if (send_int(fd, message) <= 0)
-    {
+ */
+void send_int_check(int fd, uint32_t message) {
+    if (send_int(fd, message) <= 0) {
         connection_failure();
     }
 }
@@ -62,15 +59,13 @@ void send_int_check(int fd, uint32_t message)
 /**
  * Receives the next int sent by the server. Gracefully terminates the
  * client if no connection could be made.
- * 
+ *
  * fd: the file descriptor for the socket.
  * response: an int pointer that the response will be written to.
- * 
- */ 
-void recv_int_check(int fd, uint32_t* response)
-{
-    if (recv_int(fd, response) <= 0)
-    {
+ *
+ */
+void recv_int_check(int fd, uint32_t* response) {
+    if (recv_int(fd, response) <= 0) {
         connection_failure();
     }
 }
@@ -78,21 +73,19 @@ void recv_int_check(int fd, uint32_t* response)
 /**
  * Sends a string prepended by a protocol to the server. Gracefully terminates
  * the client if no connection could be made/
- * 
+ *
  * fd: the file descriptor for the socket
  * protocol: a single-digit between 1 and 9 inclusive specifying the type of
  *           message that is being sent (see common.h)
  * message: the string to be sent
  */
-void send_string_check(int fd, int protocol, char* message)
-{
+void send_string_check(int fd, int protocol, char* message) {
     char* packet = calloc(strlen(message) + 1, sizeof(char));
     packet[0] = itoc(protocol);
     strcat(packet, message);
     int bytes_sent = send_string(fd, packet);
     free(packet);
-    if (bytes_sent <= 0)
-    {
+    if (bytes_sent <= 0) {
         connection_failure();
     }
 }
@@ -100,78 +93,67 @@ void send_string_check(int fd, int protocol, char* message)
 /**
  * Receives the next string sent by the server. Gracefully terminates
  * the client if no connection could be made
- * 
+ *
  * fd: the file descriptor for the socket
  * response: a string pointer that the response will be written to (memory must
  * be freed by the calling function)
  */
-void recv_string_check(int fd, char** response)
-{
-    if (recv_string(fd, response) <= 0)
-    {
+void recv_string_check(int fd, char** response) {
+    if (recv_string(fd, response) <= 0) {
         connection_failure();
     }
 }
 
 /**
  * checks if a coordinate is within the limits of the field
- * 
+ *
  * x: the x-coordinate
  * y: the y-coordinate
- * 
+ *
  * returns: 1 if the coordinate was valid, otherwise 0
- */ 
-bool valid_coord(int x, int y)
-{
+ */
+bool valid_coord(int x, int y) {
     return x >= 0 && x < NUM_TILES_X && y >= 0 && y < NUM_TILES_Y;
 }
 
 /**
  * updates a field tile at a coordinate with a new character
- * 
+ *
  * gs: The gamestate struct containing the field
  * x: the x-coordinate
  * y: the y-coordinate
  * c: the new character
- */ 
-void update_tile(GameState_t* gs, int x, int y, const char c)
-{
-    if (DEBUG)
-    {
+ */
+void update_tile(GameState_t* gs, int x, int y, const char c) {
+    if (DEBUG) {
         printf("Attempting to update x:%d y:%d with %c\n", x, y, c);
     }
-    if (!valid_coord(x, y))
-    {
+    if (!valid_coord(x, y)) {
         printf("Invalid field update.\n");
-    }
-    else
-    {
+    } else {
         gs->field[x][y] = c;
-    }   
+    }
 }
 
 /**
  * draws the field on the terminal
- * 
+ *
  * gs: the gamestate struct containing the field
- */ 
-void draw_field(GameState_t* gs)
-{
+ */
+void draw_field(GameState_t* gs) {
     printf("\n\n\nRemaining mines: %d\n\n ", gs->remaining_mines);
 
     for (int x = 0; x < NUM_TILES_X; x++)
-        printf(" %d", x+1);
+        printf(" %d", x + 1);
     printf("\n");
 
     for (int x = 0; x < NUM_TILES_X * 2 + 1; x++)
         printf("-");
     printf("\n");
 
-    for (int y = 0; y < NUM_TILES_Y; y++)
-    {
+    for (int y = 0; y < NUM_TILES_Y; y++) {
         printf("%c|", 65 + y);
-        for (int x = 0; x < NUM_TILES_X; x++)
-        {
+        for (int x = 0; x < NUM_TILES_X; x++) {
             char c = gs->field[x][y];
             printf("%c ", c ? c : ' ');
         }
@@ -182,54 +164,51 @@ void draw_field(GameState_t* gs)
 
 /**
  * gets the next move from the user
- * 
+ *
  * x: a pointer to an x-coordinate, updated by the function
  * y: a pointer to a y-coordinate, updated by the function
- * 
+ *
  * returns: an int representing the option selected (either QUIT,
  *          FLAG_TILE, or REVEAL_TILE)
- */ 
-int option(int* x, int* y)
-{
+ */
+int option(int* x, int* y) {
     char option, position[2];
 
     // Get option (place flag, reveal flag, or quit)
-    do
-    {
+    do {
         printf("Select option <P, R, Q>: ");
         scanf("%c", &option);
-        while(getchar() != '\n');
+        while (getchar() != '\n')
+            ;
 
-        if (option == 'Q')
-        {
+        if (option == 'Q') {
             return QUIT;
         }
     } while (!(option == 'P' || option == 'R') || isalpha(option) == 0);
 
     // Get position request if user didn't quit
-    do
-    {
+    do {
         printf("Select position: ");
         scanf("%2s", position);
-        while(getchar() != '\n');
+        while (getchar() != '\n')
+            ;
         *x = ctoi(position[1]) - 1;
         *y = itoascii(position[0]);
     } while (!valid_coord(*x, *y));
-    
+
     return option == 'P' ? FLAG_TILE : REVEAL_TILE;
 }
 
 /**
  * plays a game of Minesweeper
- * 
+ *
  * fd: file descriptor to the socket for server communication
- * 
+ *
  * returns: the result of the game (QUIT, WIN, or LOSE)
  */
-int game(int fd)
-{
+int game(int fd) {
     GameState_t gs = {0};
-    
+
     // Request to play game
     send_int_check(fd, PLAY);
     // Get the initial mine count
@@ -239,62 +218,54 @@ int game(int fd)
     bool gameover = false;
     char* response;
     draw_field(&gs);
-    
+
     // Query next move until user quits
-    while((protocol = option(&x_pos, &y_pos)) != QUIT)
-    {
+    while ((protocol = option(&x_pos, &y_pos)) != QUIT) {
         // Tell the server the user's move
         char pos_request[] = {itoc(x_pos), itoc(y_pos)};
         send_string_check(fd, protocol, pos_request);
-        
+
         // Get the first response from the server. Interpret the
         // response differently based on whether the user flagged
         // or revealed.
         recv_string_check(fd, &response);
-        switch (protocol)
-        {
-            case FLAG_TILE:
-                // There was no mine 
-                if(response[0] == TERMINATOR)
-                {
-                    printf("\nNo mine at that position!\n");
+        switch (protocol) {
+        case FLAG_TILE:
+            // There was no mine
+            if (response[0] == TERMINATOR) {
+                printf("\nNo mine at that position!\n");
+            }
+            // User correctly flagged a mine
+            else {
+                recv_int_check(fd, &gs.remaining_mines);
+                update_tile(&gs, x_pos, y_pos, FLAG);
+            }
+            break;
+        case REVEAL_TILE:
+            // Reveal all tiles the server sends back
+            while (response[0] != TERMINATOR) {
+                char response_x = ctoi(response[0]);
+                char response_y = ctoi(response[1]);
+                char response_char = response[2];
+                // Game is over if the server sends a mine.
+                if (response_char == MINE) {
+                    gameover = true;
                 }
-                // User correctly flagged a mine
-                else
-                {
-                    recv_int_check(fd, &gs.remaining_mines);
-                    update_tile(&gs, x_pos, y_pos, FLAG);
-                }
-                break;
-            case REVEAL_TILE:
-                // Reveal all tiles the server sends back
-                while(response[0] != TERMINATOR)
-                {
-                    char response_x = ctoi(response[0]);
-                    char response_y = ctoi(response[1]);
-                    char response_char = response[2];
-                    // Game is over if the server sends a mine.
-                    if(response_char == MINE)
-                    {
-                        gameover = true;
-                    }
-                    update_tile(&gs, response_x, response_y, response_char);
-                    recv_string_check(fd, &response);
-                }
+                update_tile(&gs, response_x, response_y, response_char);
+                recv_string_check(fd, &response);
+            }
 
-                break;
-            default:
-                printf("Client protocol error! Consult programmers!\n");
-                break;
+            break;
+        default:
+            printf("Client protocol error! Consult programmers!\n");
+            break;
         }
         draw_field(&gs);
-        if (gs.remaining_mines == 0)
-        {
+        if (gs.remaining_mines == 0) {
             printf("You won!!!\n");
             return WIN;
         }
-        if (gameover)
-        {
+        if (gameover) {
             printf("Game over!!!\n");
             return LOSE;
         }
@@ -303,44 +274,36 @@ int game(int fd)
     return QUIT;
 }
 
-
 /**
  * Compares two high scores to determine their relative position with respect
- * to the following ordering (from the spec): 
- * 
+ * to the following ordering (from the spec):
+ *
  * Highs scores are displayed in descending order of the number of seconds each
  * successful game took to complete. If two or more games have the same number
  * of seconds then the game played by the player with the highest number of
  * games won are displayed last. If two or more games were won in the
  * same number of seconds by players with the same number of games won then
  * display those games by the names of their players in alphabetical order.
- * 
+ *
  * highscore_a: A pointer to a HighScore_t, to be compared against highscore_b.
  * highscore_b: A pointer to a HighScore_t, to be compared against highscore_a.
- * 
- * returns: an int representing the position of highscore_a relative to 
+ *
+ * returns: an int representing the position of highscore_a relative to
  * highscore_b, with respect to the ordering described above.
- * 
- */ 
-int compare_highscores(const void* highscore_a, const void* highscore_b)
-{
+ *
+ */
+int compare_highscores(const void* highscore_a, const void* highscore_b) {
     HighScore_t* a = (HighScore_t*)highscore_a;
     HighScore_t* b = (HighScore_t*)highscore_b;
-    if (a->best_time > b->best_time)
-    {
+    if (a->best_time > b->best_time) {
         return -1;
-    }
-    else if (a->best_time < b->best_time)
-    {
+    } else if (a->best_time < b->best_time) {
         return 1;
     }
-    
-    if (a->games_won < b->games_won)
-    {
+
+    if (a->games_won < b->games_won) {
         return -1;
-    }
-    else if (a->games_won > b->games_won)
-    {
+    } else if (a->games_won > b->games_won) {
         return 1;
     }
 
@@ -349,37 +312,33 @@ int compare_highscores(const void* highscore_a, const void* highscore_b)
 
 /**
  * Gets and prints the server leaderboard
- * 
+ *
  * fd: file descriptor to the socket for server communication
- * 
- */ 
-void leaderboard(int fd)
-{
+ *
+ */
+void leaderboard(int fd) {
     // Request leaderboard from server
-    send_int_check(fd, LEADERBOARD);   
+    send_int_check(fd, LEADERBOARD);
 
     // Get leaderboard size from server
     uint32_t size;
     recv_int_check(fd, &size);
 
     // If leaderboard is empty, tell the user
-    if (size == 0)
-    {
+    if (size == 0) {
         printf(
             "============================================================\n"
             "There is no information currently stored in the leaderboard."
             "Try again later.\n"
-            "============================================================\n"
-        );
+            "============================================================\n");
     }
 
     // Get an unordered list of highscores from the server
     HighScore_t* highscores = malloc(sizeof(HighScore_t) * size);
     char* response;
-    for (int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++) {
         recv_string_check(fd, &response);
-        highscores[i].name = strtok(response, DELIM);;
+        highscores[i].name = strtok(response, DELIM);
         highscores[i].best_time = atoi(strtok(NULL, DELIM));
         highscores[i].games_won = atoi(strtok(NULL, DELIM));
         highscores[i].games_played = atoi(strtok(NULL, DELIM));
@@ -392,13 +351,10 @@ void leaderboard(int fd)
 
     // Print the ordered high scores
     printf("============================================================\n");
-    for (int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++) {
         printf("%-10s \t %10d seconds \t %d games won, %d games played\n",
-                highscores[i].name,
-                highscores[i].best_time,
-                highscores[i].games_won,
-                highscores[i].games_played);
+               highscores[i].name, highscores[i].best_time,
+               highscores[i].games_won, highscores[i].games_played);
     }
     printf("============================================================\n");
 
@@ -408,39 +364,35 @@ void leaderboard(int fd)
 
 /**
  * Logs in to a Minesweeper server
- * 
+ *
  * ip: a string representing the IP address of the server
  * port: an int representing the port of the server
- * 
+ *
  * returns: a file descriptor to the socket for server communication
- */ 
-int login(const char* ip, int port)
-{
+ */
+int login(const char* ip, int port) {
     printf("\nConnecting to %s:%d...\n", ip, port);
-    
-    struct sockaddr_in serv_addr; 
+
+    struct sockaddr_in serv_addr;
     int fd;
 
     // Setup a socket to be used for connection
-    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-    {
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\nSocket creation error.\n");
         exit(1);
     }
 
     // Construct server information based on ip and port
-    memset(&serv_addr, '0', sizeof(serv_addr)); 
-    serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(port); 
-    if(inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0)  
-    {
-        printf("\nInvalid address or address not supported.\n"); 
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address or address not supported.\n");
         exit(1);
     }
 
     // Connect to the server
-    if (connect(fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    {
+    if (connect(fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
         printf("\nConnection failed.\n");
         exit(1);
     }
@@ -450,8 +402,7 @@ int login(const char* ip, int port)
     // Wait in queue until server is not busy
     uint32_t play_response;
     recv_int_check(fd, &play_response);
-    if (play_response != PLAY)
-    {
+    if (play_response != PLAY) {
         printf("Server is at capacity. You have been placed into a queue...\n");
         recv_int_check(fd, &play_response);
     }
@@ -461,23 +412,22 @@ int login(const char* ip, int port)
     char username[10];
     char password[10];
     printf("Enter your username: ");
-    scanf(" %10s", username);  
+    scanf(" %10s", username);
     printf("Enter your password: ");
     scanf(" %10s", password);
     char credentials[22];
-    sprintf(credentials,"%s:%s", username, password);
-    
+    sprintf(credentials, "%s:%s", username, password);
+
     // Send login credentials to server
     send_string_check(fd, LOGIN, credentials);
 
     // Continue to main menu if server authenticated the credentials
     uint32_t response;
     recv_int_check(fd, &response);
-    if (response)
-    {
+    if (response) {
         return fd;
     }
-    
+
     printf("Login failure.\n");
     exit(1);
 }
@@ -485,48 +435,41 @@ int login(const char* ip, int port)
 /**
  * Program entrypoint. Takes the Minesweeper server IP address and port as
  * arguments
- * 
+ *
  * Usage: ./client.o ip port
  */
-int main(int argc, char* argv[])
-{
-    if (argc != 3)
-    {
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
         fprintf(stderr, "Invalid usage! Should be: ./client.o [ip] [port]\n");
         return 1;
     }
- 
+
     const char* ip = argv[1];
     int port = atoi(argv[2]);
     int fd = login(ip, port);
-       
+
     int selection;
     // Present main menu to client until they quit (menu option 3)
-    do
-    {
-        printf
-        (
-            "\nWelcome to the Minesweeper gaming system.\n\n"
-            "Please enter a selection:\n"
-            "<1> Play Minesweeper\n"
-            "<2> Show Leaderboard\n"
-            "<3> Quit\n\n"
-            "Selection option (1-3): "
-        );
+    do {
+        printf("\nWelcome to the Minesweeper gaming system.\n\n"
+               "Please enter a selection:\n"
+               "<1> Play Minesweeper\n"
+               "<2> Show Leaderboard\n"
+               "<3> Quit\n\n"
+               "Selection option (1-3): ");
 
         scanf("%d", &selection);
-        while(getchar() != '\n');
+        while (getchar() != '\n');
 
-        switch (selection)
-        {
-            // Play (menu option 1)
-            case 1:
-                game(fd);
-                break;
-            // Leaderboard (menu option 2)
-            case 2:
-                leaderboard(fd);
-                break;
+        switch (selection) {
+        // Play (menu option 1)
+        case 1:
+            game(fd);
+            break;
+        // Leaderboard (menu option 2)
+        case 2:
+            leaderboard(fd);
+            break;
         }
     } while (selection != 3);
 
--- common.c
+++ common.c
@@ -1,32 +1,31 @@
 /*******************************************************************************
  * commonc.c
- * 
+ *
  * Common functions used for server-client communication.
- *  
+ *
  * Author: Benjamin Saljooghi n9448233
  */
 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
-#include <unistd.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #include "common.h"
 
 /**
  * Sends an int via the socket at the given file descriptor. Guarantees
  * network byte order.
- * 
+ *
  * fd: the file descriptor for the socket
  * message: an int to be sent
- * 
+ *
  * returns: number of bytes sent or -1 if error
- */ 
-int send_int(int fd, uint32_t message)
-{
+ */
+int send_int(int fd, uint32_t message) {
     message = htonl(message);
     return write(fd, &message, sizeof(uint32_t));
 }
@@ -34,14 +33,13 @@ int send_int(int fd, uint32_t message)
 /**
  * Receives the next int via the socket at the given file descriptor. The int
  * received is translated from network byte order to host byte order.
- * 
+ *
  * fd: the file descriptor for the socket
  * response: an int pointer that the response will be written to.
- * 
+ *
  * returns: number of bytes received or -1 if error
- */ 
-int recv_int(int fd, uint32_t* response)
-{
+ */
+int recv_int(int fd, uint32_t* response) {
     int bytes_read = read(fd, response, sizeof(uint32_t));
     *response = ntohl(*response);
     return bytes_read;
@@ -51,17 +49,15 @@ int recv_int(int fd, uint32_t* response)
  * Sends a string via the socket at the given file descriptor. The string is
  * sent by first sending an int representing the byte length of the string,
  * followed by the string itself.
- * 
+ *
  * fd: the file descriptor for the socket
  * message: a string to be sent
- * 
+ *
  * returns: number of bytes sent or -1 if error
- */ 
-int send_string(int fd, char* message)
-{
+ */
+int send_string(int fd, char* message) {
     int bytes_sent = send_int(fd, strlen(message));
-    if (bytes_sent <= 0)
-    {
+    if (bytes_sent <= 0) {
         return bytes_sent;
     }
     return write(fd, message, strlen(message));
@@ -71,19 +67,17 @@ int send_string(int fd, char* message)
  * Receives a string via the socket at the given file descriptor. The string is
  * received by first receiving an int representing the byte length of the
  * incoming string, followed by the string itself.
- * 
+ *
  * fd: the file descriptor for the socket
  * response: a string pointer that the response will be written to (memory must
  * be freed by the calling function)
- * 
- * returns: number of bytes received or -1 if error 
- */ 
-int recv_string(int fd, char** response)
-{
+ *
+ * returns: number of bytes received or -1 if error
+ */
+int recv_string(int fd, char** response) {
     uint32_t length;
     int bytes_read = recv_int(fd, &length);
-    if (bytes_read <= 0)
-    {
+    if (bytes_read <= 0) {
         return bytes_read;
     }
     *response = calloc(length + 1, sizeof(char));
